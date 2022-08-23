41ec633a4e4e21fc18575650dadb4537544b9e02
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-08-23 10:24:03 -0600

    adds imgui support

--- Ember/Sandbox/TestLayer.cpp
+++ Ember/Sandbox/TestLayer.cpp
@@ -7,12 +7,13 @@ TestLayer::TestLayer()
 TestLayer::~TestLayer()
 {
 	delete m_shader;
+
 }
 
 void TestLayer::onAttach(Window* win)
 {
 	m_win = win;
-	m_shader = new Shader(".\\Src\\Shaders\\basicShader.hlsl");
+	m_shader = new Shader("../Src/Shaders/basicShader.hlsl");
 
 	glGenVertexArrays(1, &m_vao);
 	glBindVertexArray(m_vao);
@@ -30,7 +31,8 @@ void TestLayer::onAttach(Window* win)
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
 	glEnableVertexAttribArray(0);
-	
+
+	m_triColor = { 0.34f, 0.54f, 0.72f };
 }
 
 void TestLayer::onDetach()
@@ -47,7 +49,29 @@ void TestLayer::onUpdate(double dt)
 void TestLayer::onRender()
 {
 	m_shader->use();
+	m_shader->setVec3("uColor", m_triColor);
 	glBindVertexArray(m_vao);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glBindVertexArray(NULL);
+}
+
+void TestLayer::onImguiRender()
+{
+	ImGui::Begin("Main Window");
+
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("This is a menu button"))
+		{
+			if (ImGui::MenuItem("Hello!"))
+			{
+
+			}
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+	}
+	ImGui::ColorEdit3("Triangle Color", &m_triColor[0]);
+
+	ImGui::End();
 }
--- Ember/Sandbox/TestLayer.h
+++ Ember/Sandbox/TestLayer.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "../Src/Core/Layer.h"
 #include "../Src/Graphics/Shader.h"
+#include "../Gui/gui.h"
+#include "../Math/glm.hpp"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -14,9 +16,11 @@ public:
 	virtual void onDetach();
 	virtual void onUpdate(double dt);
 	virtual void onRender();
+	virtual void onImguiRender();
 
 private:
 	Window* m_win;
 	Shader* m_shader;
 	unsigned int m_vao;
+	glm::vec3 m_triColor;
 };
